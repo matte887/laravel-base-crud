fd43cad8fcea603cde2157849ec09c17dd4d65dc
matte887 <m.erroi92@gmail.com>
2022-06-30 16:28:15 +0200

    Installato bootstrap + base layout

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Comic;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -13,7 +14,8 @@ class PageController extends Controller
      */
     public function index()
     {
-        //
+        $comics_list = Comic::all();
+        return view('comics.index', $comics_list);
     }
 
     /**
