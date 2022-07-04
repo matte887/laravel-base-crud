0a192c2d0aa118cd72d388016b87034ff51c80b6
matte887 <m.erroi92@gmail.com>
2022-07-04 16:53:28 +0200

    Implementato soft delete

--- app/Comic.php
+++ app/Comic.php
@@ -1,11 +1,12 @@
 <?php
-
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comic extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'title',
         'description',
